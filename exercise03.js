fba55ba19e762b06b4519278d90d8fe4e20650e2
luuvanthai <chantin947@gmail.com>
2023-11-23 08:05:07 +0700

    add exercise

---
+++ exercise03.js
@@ -0,0 +1,33 @@
+//var chemistry = window.prompt ("Nhập điểm Hoá : ")
+//var physics = window.prompt ("Nhập điểm Lý : ")
+// ép dữ liệu nhanh ( thêm + trước prompt)
+//var math = +prompt ("Nhập điểm Toán : ")
+//var math = +prompt ("Nhập điểm Toán : ")
+
+//console.log(typeof chemistry);
+//console.log(typeof physics);
+//console.log(typeof math);
+
+//var average = (Number(chemistry) + Number(physics) + Number(math)) / 3;
+//console.log("Điểm trung bình của 3 môn là : ", average);
+
+// Modal xác nhận từ người dùng
+// Yêu cầu người nhập vào tuổi, nếu tuổi lớn hơn 18 và là con trai thì hiển thị thông báo là đủ điều kiện, 
+// ngược lại k đủ điều kiện
+
+//var yearOld = +prompt ("Nhập tuổi của bạn :");;
+//var gender =  prompt ("Nhập giới tính của bạn :")
+//if (yearOld > 18 && gender === "Nam") {
+  //  alert("Đủ điều kiện");
+//} else {""
+  //  alert("Không đủ điều kiện");
+//}
+
+//những giá trị nhận fasle trong js
+// 1 chuỗi rỗng, false. null, 0, NaN
+var value = 0 ;
+if (value) {
+    console.log("Điều kiện đúng");
+} else {
+    console.log("Điều kiện sai");
+}
